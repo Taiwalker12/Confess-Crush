478bfa9dab0a1cc1599c6513d960cd0aa6867c18
Taiwalker12 <91471675+Taiwalker12@users.noreply.github.com>
2021-09-27 21:59:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người người yêu Phạm Đức Tài á không nói lêu lêu',
+    titleWeb: "Demo",
+    introTitle: 'Tên người người yêu Nhím Ú Nu á không nói lêu lêu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
