2ebcd8a81124060fd137db7fa43e0215a9072a7b
Taiwalker12 <91471675+Taiwalker12@users.noreply.github.com>
2021-10-02 17:53:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Demo",
-    introTitle: 'Chào cậu j đó dễ thương nha ^^ ',
+    introTitle: 'Chào cậu j đó dễ thương nha ^^ .',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
