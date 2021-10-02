0d2e3158e588003363cc8e79d508c80498367652
Taiwalker12 <91471675+Taiwalker12@users.noreply.github.com>
2021-10-02 18:02:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
     titleWeb: "Demo",
-    introTitle: 'Tên người người yêu mk  á không nói đâu',
+    introTitle: ' Chào cậu j dễ thương nha ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Đằng sau chữ yêu đây là thương Tài :33`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
