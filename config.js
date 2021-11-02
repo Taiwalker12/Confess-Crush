5ff29593976a0ee4209a826df979341ad7ce4189
Taiwalker12 <91471675+Taiwalker12@users.noreply.github.com>
2021-11-02 18:13:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Demo",
-    introTitle: 'Chào cậu j đó dễ thương nha ^^ .',
+    introTitle: 'Chào cậu j đó dễ thương nha ^^ - Phạm Đức Tài.',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -8,13 +8,13 @@ const CONFIG = {
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'Thích Tài  <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu mk Tài thoi  <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha baby.',
+    btnAccept: 'Okiiiii lun bn iu <3',
     messLink: 'https://github.com/Taiwalker12/Confess-Crush'//
 }
